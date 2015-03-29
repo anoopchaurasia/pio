c7146b3214dd2adbb05f21678111717f8b878f82
anoopchaurasia <anoop26667@gmail.com>
2015-03-29 18:34:45 +0530

    hello

--- addEvent.js
+++ addEvent.js
@@ -6,9 +6,12 @@ process.on('uncaughtException', function(e){
 function addEvent(event_id) {
 	require("request")
 	.get("http://"+config.eventzio.host+":"+config.eventzio.port+"/events/"+event_id+".json", function (err, response) {
-		if(response.statusCode == 404 && event_id > 450) {
-			console.log(response.body);
-			process.exit(0);
+		if(response.statusCode == 404) {
+			if(event_id > 450)
+				console.log(response.body);
+				process.exit(0);
+				return;
+			}
 			addEvent(++event_id);
 			return;
 		}
