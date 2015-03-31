68aa4a7be970249b45b34543a398a9efec3fd9d7
anoopchaurasia <anoop26667@gmail.com>
2015-03-31 18:04:29 +0530

    added communication key

--- node/Base.js
+++ node/Base.js
@@ -7,7 +7,14 @@ fm.AbstractClass("Base");
 Base = function (me){this.setMe=function(_me){me=_me;};
 
     this.Abstract.method = function(){};
-    this.POST = function(req, resp, method){
+    this.POST = function(req, resp, method, query_parts){
+      if(req.method === "POST") {
+        if(query_parts.communication_key != "no_access_to_you_dude") {
+          resp.end("good bye");
+          console.log("post request without communication_key");
+          return
+        }
+      }
        var method = method || 'method';
        var cls = this.getSub();
        console.log("method", method, req.params);
@@ -16,6 +23,6 @@ Base = function (me){this.setMe=function(_me){me=_me;};
 
     this.GET = function(req, resp, method){
         console.log("GET");
-        this.POST(req, resp, method);
+        this.POST(req, resp, method, true);
     };
 };
